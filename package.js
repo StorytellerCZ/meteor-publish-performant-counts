8e7943aeb99c06a1c75ca1442d53daf9d09af024
Jan Dvorak <jdvorak@literaryuniverse.com>
2024-05-11 13:41:55 +0200

    Updates for Meteor 3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'natestrauser:publish-performant-counts',
-  version: '0.1.2',
+  version: '0.2.0',
   // Brief, one-line summary of the package.
   summary: 'Publish counts of large collections efficently',
   // URL to the Git repository containing the source code for this package.
@@ -8,11 +8,12 @@ Package.describe({
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
-});
+})
 
-Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.1');
-  api.addFiles('lib/server.js', 'server');
-  api.addFiles('lib/client.js', 'client');
-  api.export(['Counter']);
-});
+Package.onUse(function (api) {
+  api.versionsFrom('3.0-rc.1')
+  api.use('ecmascript')
+  api.addFiles('lib/server.js', 'server')
+  api.addFiles('lib/client.js', 'client')
+  api.export(['Counter'])
+})
