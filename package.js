5431f1b5c9eea3148934547845ba4930816f8b20
Jan Dvorak <jdvorak@literaryuniverse.com>
2024-06-13 14:46:24 +0200

    Add mongo dependency

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'natestrauser:publish-performant-counts',
   version: '0.2.0',
   // Brief, one-line summary of the package.
-  summary: 'Publish counts of large collections efficently',
+  summary: 'Publish counts of large collections efficiently',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/nate-strauser/meteor-publish-performant-counts',
   // By default, Meteor will default to using README.md for documentation.
@@ -12,7 +12,7 @@ Package.describe({
 
 Package.onUse(function (api) {
   api.versionsFrom('3.0-rc.3')
-  api.use('ecmascript')
+  api.use(['ecmascript', 'mongo'])
   api.addFiles('lib/server.js', 'server')
   api.addFiles('lib/client.js', 'client')
   api.export(['Counter'])
